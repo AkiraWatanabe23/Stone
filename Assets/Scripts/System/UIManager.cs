ba20713def25313552b3f5a38e512111a6903530
Akira Watanabe <a0323w@gmail.com>
2023-02-19 21:04:34 +0900

    [add] UIをキーボードで操作する

---
+++ Assets/Scripts/System/UIManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Selectable _startElement = default;
+
+    private Selectable _currentElement = default;
+
+    private void Start()
+    {
+        if (_startElement != null)
+        {
+            _currentElement = _startElement;
+            _currentElement.Select();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (_currentElement != null)
+                _currentElement.OnDeselect(null);
+
+            if (Input.GetKey(KeyCode.LeftShift) ||
+                Input.GetKey(KeyCode.RightShift))
+                _currentElement = _currentElement.FindSelectableOnUp();
+            else
+                _currentElement = _currentElement.FindSelectableOnDown();
+
+            if (_currentElement != null)
+                _currentElement.Select();
+        }
+    }
+}
