d5e560f72e4a2c3d7bf19a7a127ea25310e41572
Akira Watanabe <a0323w@gmail.com>
2023-02-21 17:52:42 +0900

    [add] キーボード操作でマスを選択したい

---
+++ Assets/Scripts/Player/StoneSelect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneSelect : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void Select()
+    {
+        KeyCode input = default;
+
+        switch (input)
+        {
+            case KeyCode.UpArrow:
+            case KeyCode.W:
+                break;
+            case KeyCode.DownArrow:
+            case KeyCode.D:
+                break;
+            case KeyCode.LeftArrow:
+            case KeyCode.A:
+                break;
+            case KeyCode.RightArrow:
+            case KeyCode.S:
+                break;
+        }
+    }
+}
