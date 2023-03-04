3a74a2cb64010d5feadbdfeb09cef307339d8ce6
Akira Watanabe <a0323w@gmail.com>
2023-03-04 14:21:33 +0900

    [update] UI設置中...

---
+++ Assets/Scripts/System/GameStart.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameStart : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _startEvent = default;
+    [SerializeField] private UnityEvent _closeEvent = default;
+
+    private bool _isStart = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _isStart)
+        {
+            Debug.Log("始めます");
+            _startEvent?.Invoke();
+        }
+        else if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            _closeEvent?.Invoke();
+        }
+    }
+
+    public void Startable()
+    {
+        _isStart = !_isStart;
+    }
+}
