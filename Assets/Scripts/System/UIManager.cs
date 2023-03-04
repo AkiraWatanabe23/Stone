56da784140ad7bfc4a527692acaef486e1a3af1c
Akira Watanabe <a0323w@gmail.com>
2023-03-04 15:22:00 +0900

    [update] UI関連の処理を一つにまとめた

--- Assets/Scripts/System/UIManager.cs
+++ Assets/Scripts/System/UIManager.cs
@@ -6,7 +6,9 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Text _turn = default;
     [SerializeField] private Text _whichPlace = default;
     [SerializeField] private Image _moveSelect = default;
+    [SerializeField] private Selectable _startElement = default;
 
+    private Selectable _currentElement = default;
     private GameManager _manager = default;
 
     public Image MoveSelect { get => _moveSelect; protected set => _moveSelect = value; }
@@ -15,11 +17,32 @@ public class UIManager : MonoBehaviour
     {
         _manager = GetComponent<GameManager>();
         _moveSelect.gameObject.SetActive(true);
+
+        if (_startElement != null)
+        {
+            _currentElement = _startElement;
+            _currentElement.Select();
+        }
     }
 
     private void Update()
     {
         _turn.text = _manager.Turn.ToString();
         _whichPlace.gameObject.SetActive(!_moveSelect.gameObject.activeSelf);
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (_currentElement != null)
+                _currentElement.OnDeselect(null);
+
+            if (Input.GetKey(KeyCode.LeftShift) ||
+                Input.GetKey(KeyCode.RightShift))
+                _currentElement = _currentElement.FindSelectableOnUp();
+            else
+                _currentElement = _currentElement.FindSelectableOnDown();
+
+            if (_currentElement != null)
+                _currentElement.Select();
+        }
     }
 }
