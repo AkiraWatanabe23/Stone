f052b01da66df3547812cccd6030fc63b5f9c4e1
Akira Watanabe <a0323w@gmail.com>
2023-03-06 13:03:13 +0900

    [update] Skyboxの回転

---
+++ Assets/Scripts/System/RotateSkybox.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Skyboxを回転させる
+/// </summary>
+public class RotateSkybox : MonoBehaviour
+{
+    [SerializeField] private float _rotateSpeed = 0.1f;
+
+    private float _angle = 0f;
+
+    private void Update()
+    {
+        _angle += _rotateSpeed;
+        RenderSettings.skybox.SetFloat("_Rotation", _angle);
+    }
+}
